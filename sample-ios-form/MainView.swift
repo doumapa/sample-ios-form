cea7a4457b410b20e94fa2dba571eff1d194b636
Makoto Kaneko <s.makoto.kaneko@iridge.jp>
2018-06-01 19:06:47 +0900

    MainView クラスを追加

---
+++ sample-ios-form/MainView.swift
@@ -0,0 +1,29 @@
+//
+//  MainView.swift
+//  sample-ios-form
+//
+//  Created by makoto.kaneko on 2018/06/01.
+//  Copyright © 2018年 makoto.kaneko. All rights reserved.
+//
+
+import UIKit
+
+class MainView: UIView {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    
+    
+    
+    
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
